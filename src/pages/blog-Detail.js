dded79bdfd40c4612469e0f7f199be11ef5db948
shyan <m15205519363@163.com>
2020-11-15 01:46:12 +0800

    fix issue

--- src/pages/blog-Detail.js
+++ src/pages/blog-Detail.js
@@ -1,18 +1,22 @@
-import React, { useState, useEffect } from "react"
+import React, {useState, useEffect } from "react"
 import Layout from "../components/layout"
 // import detailsStyles from "./details.module.css"
 import QueueAnim from "rc-queue-anim"
 
 export default ({ location }) => {
     console.log("***",location);
+    const [blogTitle, setBlogTitle] = useState('');
+
   useEffect(() => {
     document.getElementById("detailBody").innerHTML = location.state.node.html
+    // blogTitle=location.state.node.frontmatter.title;
+    setBlogTitle(location.state.node.frontmatter.title);
   })
 
   return (
     <Layout>
       <QueueAnim key="queueAnim" delay={2000} duration={2000} opacity={[1, 0]}>
-        <div>{location.state.node.frontmatter.title}</div>
+        <div>{blogTitle}</div>
         <div id="detailBody"></div>
       </QueueAnim>
     </Layout>
